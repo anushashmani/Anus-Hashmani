121c575599208029d4845cd74117c8ea4e502202
Anus Shafi <anushashmani10@gmail.com>
2024-05-30 20:35:27 +0500

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,268 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  pixelhouse,
+  strugbits,
+  davinci,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  // {
+  //   id: "project",
+  //   title: "Project",
+  // },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "WordPress Developer",
+    company_name: "Pixel House Technologies Pvt Ltd.",
+    icon: pixelhouse,
+    iconBg: "#383E56",
+    date: "June 2023 - Current",
+    points: [
+      "Spearheaded the development of a new software module, reducing processing time by 30% and increasing overall system efficiency",
+      "Implemented best practices in software testing, resulting in a 20% reduction in bug incidents and improving product quality.",
+      "Played a key role in client onboarding and support activities, ensuring smooth implementation and customer satisfaction.",
+    ],
+  },
+  {
+    title: "WordPress Developer intern",
+    company_name: "Strugbits",
+    icon: strugbits,
+    iconBg: "#E6DEDD",
+    date: "FEBUARY 2023 – MAY 2023",
+    points: [
+      "Developed custom WordPress themes and plugins tailored to client requirements, resulting in enhanced website functionality and user experience.",
+      "Implemented responsive design principles, ensuring seamless compatibility across various devices and screen sizes.",
+      "Optimized website performance by leveraging caching techniques, image optimization, and code minification, resulting in improved loading times and user engagement",
+      "Optimized website performance by leveraging caching techniques, image optimization, and code minification, resulting in improved loading times and user engagement.",
+    ],
+  },
+  {
+    title: "WordPress Developer",
+    company_name: "Da Vinci Solutions",
+    icon: davinci,
+    iconBg: "#383E56",
+    date: "ANUGUST 2022 – NOVEMBER 2022",
+    points: [
+      "Actively engaged in ongoing learning and skill development, staying updated with the latest WordPress trends, plugins, and best practices to enhance technical proficiency.",
+      "Collaborated with designers and content creators to integrate multimedia elements and dynamic content into WordPress websites, enhancing visual appeal and user engagement",
+      "Provided support to clients and end-users, addressing technical inquiries and troubleshooting issues to ensure positive client relationships and satisfaction.",
+      "Demonstrated a strong work ethic, adaptability, and a willingness to take on new challenges, contributing to a positive and productive work environment at Da-Vinci Solution.",
+    ],
+  },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/anushashmani",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/anushashmani",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/anushashmani",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
